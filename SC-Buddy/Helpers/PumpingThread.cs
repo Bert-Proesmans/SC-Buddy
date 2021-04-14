f33897f1d28ada55d0fb52e1dadcb77baad3cfd1
Bert Proesmans <bproesmans@hotmail.com>
2021-04-14 22:35:31 +0200

    Split Thread Helper into PumpingThread and Reactive Helper

--- SC-Buddy/Helpers/PumpingThread.cs
+++ SC-Buddy/Helpers/PumpingThread.cs
@@ -10,35 +10,6 @@ using System.Windows.Threading;
 
 namespace SC_Buddy.Helpers
 {
-    static class Threading
-    {
-        /// <summary>
-        /// Find out what the hell is happening in reactive flows..
-        /// </summary>
-        /// <typeparam name="TSource">The item that's passed downstream reactive operators</typeparam>
-        /// <param name="source">Upstream reactive pipeline</param>
-        /// <param name="reference">Unique string to identify trace point</param>
-        /// <returns>Wrapped reactive pipeline</returns>
-        // SOURCE; https://stackoverflow.com/a/24349671/3620622
-        public static IObservable<TSource> Trace<TSource>(this IObservable<TSource> source, string reference)
-        {
-            int id = 0;
-            return Observable.Create<TSource>(observer =>
-            {
-                int registration = ++id;
-                void trace(string step, object? payload) =>
-                    Debug.WriteLine($"{reference}-{registration}: {step}({payload}), Thread: {Thread.CurrentThread.Name}({Thread.CurrentThread.ManagedThreadId})");
-                trace("Subscribe", string.Empty);
-
-                IDisposable disposable = source.Subscribe(
-                    v => { trace("OnNext", v); observer.OnNext(v); },
-                    e => { trace("OnError", string.Empty); observer.OnError(e); },
-                    () => { trace("OnCompleted", string.Empty); observer.OnCompleted(); });
-                return () => { trace("Dispose", string.Empty); disposable.Dispose(); };
-            });
-        }
-    }
-
     class PumpingThread : IDisposable
     {
         record ThreadState(TaskCompletionSource<Dispatcher> InitHandler, IObserver<Exception>? ErrorStream);
